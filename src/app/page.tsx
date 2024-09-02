900956015cdca4a316540c665fb62efe27a20be2
Dawid Domiński <dawid.dominski1125@gmail.com>
2024-09-02 02:03:31 +0200

    feat: initialize repo with eslint, prettier, husky and tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className="h-full w-full flex items-center justify-center text-5xl">
+      Test
+    </main>
+  )
+}
+export default Home
