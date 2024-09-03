ab768aee5eccbd94a319e77c41f6cdbc3163b664
Dawid Domiński <dawid.dominski1125@gmail.com>
2024-09-03 19:13:25 +0200

    refactor: delete unnecessary /products layout

--- src/app/(routes)/products/page.tsx
+++ src/app/(routes)/products/page.tsx
@@ -2,18 +2,24 @@ import React from 'react'
 import { Props } from './products.types'
 import useProducts from './useProducts'
 import ProductCard from '@src/features/products/components/productCard/productCard'
+import ProductsNavbar from '@src/features/products/components/productsNavbar/productsNavbar'
+import Pagination from '@src/features/products/components/pagination/pagination'
 
 const Products = ({ searchParams }: Props) => {
   const { products } = useProducts({ searchParams })
 
   return (
-    <section className='flex justify-center flex-1 overflow-y-auto'>
-      <div className='w-full max-w-[1250px] h-fit min-h-full grid grid-cols-[repeat(auto-fill,_minmax(288px,1fr))] justify-center justify-items-center align-items-center'>
-        {products.map((product) => {
-          return <ProductCard key={product.id} product={product} />
-        })}
-      </div>
-    </section>
+    <main className='size-full flex flex-col justify-between'>
+      <ProductsNavbar />
+      <section className='flex justify-center flex-1 overflow-y-auto'>
+        <div className='w-full max-w-[1250px] h-fit min-h-full grid grid-cols-[repeat(auto-fill,_minmax(288px,1fr))] justify-center justify-items-center align-items-center'>
+          {products.map((product) => {
+            return <ProductCard key={product.id} product={product} />
+          })}
+        </div>
+      </section>
+      <Pagination />
+    </main>
   )
 }
 
