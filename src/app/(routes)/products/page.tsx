a7016e03aac23682039cc308c4cd81ff8de21791
Dawid Domiński <dawid.dominski1125@gmail.com>
2024-09-04 23:01:51 +0200

    feat: add information about no result during filtering

--- src/app/(routes)/products/page.tsx
+++ src/app/(routes)/products/page.tsx
@@ -12,11 +12,15 @@ const Products = ({ searchParams }: Props) => {
     <main className='size-full flex flex-col justify-between'>
       <ProductsNavbar />
       <section className='flex justify-center items-center flex-1 overflow-y-auto pt-3 px-3'>
-        <div className='w-full max-w-[1220px] gap-y-4 gap-x-1 items-center h-fit min-h-full grid grid-cols-[repeat(auto-fill,_minmax(288px,1fr))] grid-rows-2 justify-center justify-items-center align-items-center'>
-          {products.map((product) => {
-            return <ProductCard key={product.id} product={product} />
-          })}
-        </div>
+        {products.length > 0 ? (
+          <div className='w-full max-w-[1220px] gap-y-4 gap-x-1 items-center h-fit min-h-full grid grid-cols-[repeat(auto-fill,_minmax(288px,1fr))] grid-rows-2 justify-center justify-items-center align-items-center'>
+            {products.map((product) => {
+              return <ProductCard key={product.id} product={product} />
+            })}
+          </div>
+        ) : (
+          <div className='text-4xl text-black'>No results :(</div>
+        )}
       </section>
       <ProductsPagination />
     </main>
