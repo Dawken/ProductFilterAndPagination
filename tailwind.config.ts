59402aff40ba882c360f7dd66d7cb7c5633cacab
Dawid Domiński <dawid.dominski1125@gmail.com>
2024-09-05 20:47:48 +0200

    refactor: optymalize font loading by using next/font/google

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,6 @@ const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        nunito: ['Nunito'],
-      },
       colors: {
         lightGrey: {
           DEFAULT: '#e3e3e3',
